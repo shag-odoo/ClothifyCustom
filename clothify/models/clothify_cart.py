cc90a94ce73e1c243c40baada044ab27ee14671b
shag-odoo <shag@odoo.com>
2023-05-08 18:58:30 +0530

    Added SQL Constraints

--- clothify/models/clothify_cart.py
+++ clothify/models/clothify_cart.py
@@ -1,17 +1,27 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 
 class ClothifyCart(models.Model):
     _name = 'clothify.cart'
     _description = 'Cart Model'
+    _sql_constraints = [
+        ('check_quantity', 'CHECK(cart_quantity >= 0)',
+         'Quantity Should be positive'),
+         ('check_price','CHECK(cart_price>0)',
+         'Price Should be more than zero')
+    ]
 
     # CUSTOMER DETAILS
-    customer_name=fields.Char()
+    customer_name=fields.Char(required=True)
     customer_address=fields.Text()
     customer_mobile=fields.Integer()
     cart_quantity=fields.Integer()
-    cart_total =fields.Integer()
-    product_name=fields.Char()
+    cart_total =fields.Integer(compute='_compute_cart_total')
+    product_name=fields.Many2one('clothify.product')
     cart_price=fields.Integer()
-    product_ids=fields.One2many('clothify.product','link_id')
+    # product_ids=fields.One2many('clothify.product','link_id')
 
+    @api.depends('cart_quantity','cart_price')
+    def _compute_cart_total(self):
+        for record in self:
+            record.cart_total=record.cart_quantity*record.cart_price
--- clothify/models/clothify_product.py
+++ clothify/models/clothify_product.py
@@ -4,12 +4,17 @@ from odoo import models, fields,api
 class ClothifyProduct(models.Model):
     _name = 'clothify.product'
     _description = 'Product Module'
-
+    _sql_constraints = [
+        ('check_quantity', 'CHECK(quantity >= 0)',
+         'Quantity Should be positive'),
+         ('check_price','CHECK(price>0)',
+         'Price Should be more than zero')
+    ]
     name = fields.Char(required=True)
     description = fields.Text()
     price = fields.Integer()
     size = fields.Integer()
-    quantity = fields.Integer()
+    quantity = fields.Integer(default=2)
     Active = fields.Boolean(default=True)
     color = fields.Selection(
         selection=[
@@ -40,7 +45,7 @@ class ClothifyProduct(models.Model):
     # MANY2MANY RELATIONSHIPS
     tag_ids = fields.Many2many("clothify.tags")
     # MANY2ONE RELATIONSHIP
-    link_id=fields.Many2one('clothify.product')
+    # link_id=fields.Many2one('clothify.product')
     @api.depends('state')
     def add_to_cart(self):
         for record in self:
